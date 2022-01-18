902283e10d0008e61e910f8020a17a8922113ba9
Max Base <MaxBaseCode@Gmail.Com>
2022-01-18 18:24:42 +0330

    Update ripple.js

--- ripple.js
+++ ripple.js
@@ -1,10 +1,13 @@
 // https://github.com/BaseMax/RippleEffectCSS
-[].map.call(document.querySelectorAll('[anim="ripple"]'), el => {
+document.querySelectorAll(`[effect="ripple"]`).forEach(el => {
     el.addEventListener('click', e => {
         e = e.touches ? e.touches[0] : e;
-        const r = el.getBoundingClientRect(), d = Math.sqrt(Math.pow(r.width, 2) + Math.pow(r.height, 2)) * 2;
+        const r = el.getBoundingClientRect(),
+              d = Math.sqrt(Math.pow(r.width, 2) + Math.pow(r.height, 2)) * 2;
         el.style.cssText = `--s: 0; --o: 1;`;
         el.offsetTop;
         el.style.cssText = `--t: 1; --o: 0; --d: ${d}; --x:${e.clientX - r.left}; --y:${e.clientY - r.top};`;
     });
 });
+
+
